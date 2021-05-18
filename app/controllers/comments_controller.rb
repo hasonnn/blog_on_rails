a40878c20069c45c087af0cbf87d9dc297b32f4d
Jason Chan <jchan321738@gmail.com>
2021-05-17 17:56:59 -0700

    Update Navbar and alerts

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
       def destroy
         @comment = Comment.find params[:id]
         @comment.destroy
-        redirect_to post_path(@post), notice: 'Comment deleted!'
+        redirect_to post_path(@post), alert: 'Comment deleted!'
       end
     
     private
